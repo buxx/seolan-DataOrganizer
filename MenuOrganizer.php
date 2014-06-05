7020b222a833dd9f1d917e4b416b55144a32c216
Bastien Sevajol <bastien.sevajol@xsalto.com>
2014-06-05 17:11:06 +0200

    Code formatting

--- MenuOrganizer.php
+++ MenuOrganizer.php
@@ -1,22 +1,17 @@
 <?php
 
-class MenuOrganizer
-{
+class MenuOrganizer {
   
   protected $original_menu_data;
   
-  public function __construct($menu_data)
-  {
+  public function __construct($menu_data) {
     $this->original_menu_data = $menu_data;
   }
   
-  public function getOrganized()
-  {
+  public function getOrganized() {
     $organized_menu = array();
-    foreach ($this->original_menu_data as $menu_element)
-    {
-      if ($menu_element['level'] == 1)
-      {
+    foreach ($this->original_menu_data as $menu_element) {
+      if ($menu_element['level'] == 1) {
         $organized_menu[] = $this->getMenuWithHisChilds($menu_element);
       }
     }
@@ -24,19 +19,15 @@ class MenuOrganizer
     return $organized_menu;
   }
   
-  protected function getMenuWithHisChilds($menu_element)
-  {
+  protected function getMenuWithHisChilds($menu_element) {
     $menu_element['childs'] = $this->getMenuChilds($menu_element['oid']);
     return $menu_element;
   }
     
-  protected function getMenuChilds($parent_menu_id)
-  {
+  protected function getMenuChilds($parent_menu_id) {
     $menu_childs = array();
-    foreach ($this->original_menu_data as $menu_element)
-    {
-      if ($menu_element['olinkup']->raw == $parent_menu_id)
-      {
+    foreach ($this->original_menu_data as $menu_element) {
+      if ($menu_element['olinkup']->raw == $parent_menu_id) {
         $menu_childs[] = $this->getMenuWithHisChilds($menu_element);
       }
     }
--- base/DataOrganizer.php
+++ base/DataOrganizer.php
@@ -6,8 +6,7 @@
  * Cette classe permet de transformer des données seolan dans une forme plus 
  * exploitable (voir doc de ->getFormatedData())
  */
-class DataOrganizer
-{
+class DataOrganizer {
   
   /**
    * 
@@ -40,14 +39,11 @@ class DataOrganizer
    *   )
    * )
    */
-  public function getFormatedData($non_formated_data)
-  {
+  public function getFormatedData($non_formated_data) {
     $formated_data = array();
     
-    foreach (array_keys($non_formated_data) as $non_formated_data_line_key)
-    {
-      if (strpos($non_formated_data_line_key, 'lines_') !== False)
-      {
+    foreach (array_keys($non_formated_data) as $non_formated_data_line_key) {
+      if (strpos($non_formated_data_line_key, 'lines_') !== False) {
         $formated_data = $this->getUpdatedFormatedDataForField($formated_data, $non_formated_data, $non_formated_data_line_key);
       }
     }
@@ -55,20 +51,18 @@ class DataOrganizer
     return $formated_data;
   }
   
-  protected function getUpdatedFormatedDataForField($formated_data, $non_formated_data, $non_formated_data_line_key)
-  {
-    foreach ($non_formated_data[$non_formated_data_line_key] as $key => $field)
-    {
+  protected function getUpdatedFormatedDataForField($formated_data, $non_formated_data, $non_formated_data_line_key) {
+    foreach ($non_formated_data[$non_formated_data_line_key] as $key => $field) {
       $formated_data = $this->updateFormatedDataWithField($formated_data, str_replace('lines_', '', $non_formated_data_line_key), $key, $field);
     }
     
     return $formated_data;
   }
   
-  protected function updateFormatedDataWithField($data, $field_id, $key, $field)
-  {    
-    if (!array_key_exists($key, $data))
+  protected function updateFormatedDataWithField($data, $field_id, $key, $field) {    
+    if (!array_key_exists($key, $data)) {
       $data[$key] = array();
+    }
     $data[$key] = array_merge($data[$key], array($field_id => $field));
     
     return $data;
